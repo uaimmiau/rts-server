b924f47a345d7929c360c5686fda687bb2778e09
uaimmiau <zoltanchivay2137@gmail.com>
2021-06-13 11:17:23 +0200

    Add randomness to Z axis of generated units for testing

--- src/main/kotlin/game/client.kt
+++ src/main/kotlin/game/client.kt
@@ -2,6 +2,7 @@ package game
 
 import org.eclipse.jetty.websocket.api.Session
 import server.Clients
+import kotlin.random.Random
 
 class PlayerClient(val session: Session, val nickname: String, val units: MutableList<Unit>) {
     fun spawnAllUnits() {
@@ -16,7 +17,7 @@ const val START_UNIT_COUNT = 5
 fun newPlayer(session: Session, nickname: String): PlayerClient {
     val playerUnits = mutableListOf<Unit>()
     for (i in (-START_UNIT_COUNT/2)..(START_UNIT_COUNT/2)) {
-        val newUnit = Unit(nextUnitId(), UnitType.NORMAL, Position(i * 50, 25, 0), null)
+        val newUnit = Unit(nextUnitId(), UnitType.NORMAL, Position(i * 50, 25, Random.nextInt(-50,50)), null)
         playerUnits.add(newUnit)
         Units.add(newUnit)
     }
